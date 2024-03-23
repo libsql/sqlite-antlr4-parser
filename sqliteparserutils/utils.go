5c63f680a14ef6d0de8e58ffba89b17f4e2b2dc7
Robert Zimțea <robert@zimtea.com>
2024-03-23 16:34:53 +0200

    Updates antlr package path to github.com/antlr4-go/antlr/v4
    Also inadvertedly updates the antlr runtime to the latest version.
    (4.13.0)

--- sqliteparserutils/utils.go
+++ sqliteparserutils/utils.go
@@ -1,7 +1,8 @@
 package sqliteparserutils
 
 import (
-	"github.com/antlr/antlr4/runtime/Go/antlr/v4"
+	"github.com/antlr4-go/antlr/v4"
+
 	"github.com/libsql/sqlite-antlr4-parser/sqliteparser"
 )
 
